528e504a540ff6460827e0c8bb25079834c6c0a9
Arulkumar <arunmicheal8@gmail.com>
2023-11-21 14:18:48 +0530

    Update Port v6

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const path = require('path')
 
 // const bcrypt = require('bcrypt')
 // const jwt = require('jsonwebtoken')
-// const mongoose = require('mongoose');
-// const cors = require('cors')
-// const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const bodyParser = require('body-parser');
 // const { ObjectId } = require('mongodb');
 // const router = express.Router()
 
@@ -16,39 +16,39 @@ const app = express();
 // connectDb()
 // console.log('url', process.env.MONGODB_URI)
 
-// const mongoURI = process.env.MONGODB_URI
-// console.log('url', mongoURI)
-// mongoose.connect(mongoURI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
+const mongoURI = 'mongodb+srv://arulkumar72004:9O5XC38UPek8kVQb@cluster0.jlckex6.mongodb.net/?retryWrites=true&w=majority'
+console.log('url', mongoURI)
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 
-// const userSchema = new mongoose.Schema({
-//     username: String,
-//     subject: String,
-//     message: String,
-//     checked: Boolean,
-//     TimeDate: {
-//         date: String,
-//         month: String,
-//         hours: String,
-//         minutes: String,
-//         ampm: String,
-//     }
-// });
+const userSchema = new mongoose.Schema({
+    username: String,
+    subject: String,
+    message: String,
+    checked: Boolean,
+    TimeDate: {
+        date: String,
+        month: String,
+        hours: String,
+        minutes: String,
+        ampm: String,
+    }
+});
 
-// const corsOptions = {
-//     origin: '*',
-//     credentials: true,
-//     optionSuccessStatus: 200,
-// }
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
 
 
 // app.use(express.json())
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(corsOptions))
 app.use(express.static("public"))
 // app.set('view engine', 'ejs');
 
