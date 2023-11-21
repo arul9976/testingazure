6331ebd29461cfc7d39e95e5d1ba728c02ff52c0
Arulkumar M <79500043+arul9976@users.noreply.github.com>
2023-11-21 14:58:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 
-// const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt')
 // const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose');
 const cors = require('cors')
