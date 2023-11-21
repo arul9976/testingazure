0a29f95d276323236c247c5e81cea964fdeb3355
Arulkumar <arunmicheal8@gmail.com>
2023-11-21 12:54:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.get('/', (req, res) => {
 })
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 1337
 
 app.listen(PORT, () => {
     console.log(`Listening port on ${PORT}`)
